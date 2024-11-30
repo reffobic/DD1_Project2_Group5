5545ecc97ac982025fd4ae3bb0d4497f1e67ba7a
Mohamed El Refai <133569987+reffobic@users.noreply.github.com>
2024-11-30 15:08:49 +0200

    Update paddleCtrl.v

--- paddleCtrl.v
+++ paddleCtrl.v
@@ -20,18 +20,18 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module paddleCtrl(input clk, reset, pushup, pushdown, vpos, output coord);
+module paddleCtrl(input clk, reset, pushup, pushdown, vpos, output [8:0] coord);
 
 wire upwire, downwire;
 debouncer deb1(.clk(clk), .rst(reset), .in(pushup), .out(upwire));
 debouncer deb2(.clk(clk), .rst(reset), .in(pushdown), .out(downwire));
 
 wire clk_out;
-clockDivider #(625000) clkdiv (.clk(clk), .reset(reset), .enable(1'b1), .clk_out(clk_out));
+clockDivider #(100000) clkdiv (.clk(clk), .reset(reset), .enable(1'b1), .clk_out(clk_out));
 
 wire countEn;
 assign countEn = pushup ^ pushdown;
 
-counter_x_bit #(9,480) vcount(clk_out, reset, countEn, out1, coord);
+counter_x_bit #(9,480) vcount(clk_out, reset, countEn, upwire, coord);
 
 endmodule
