724f10c1d5d68944217ec12fd8eaca03fff63514
Mohamed El Refai <133569987+reffobic@users.noreply.github.com>
2024-11-30 16:56:11 +0200

    Added counter just for the ball

--- ballCtrl.v
+++ ballCtrl.v
@@ -19,14 +19,41 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module ballCount #(parameter x = 9, n = 480)(input clk, reset, enable, updown, output reg [x-1:0] count);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= n/2;
+        else if (enable) begin
+            if (count == n-1)
+                count <= 0;
+            else if(updown)
+                count <= count + 1;
+            else
+                count <= count - 1;
+        end
+    end
+endmodule
 
-module ballCtrl(input clk, reset, vCol, hCol, enable, output [9:0] xCoord, output [8:0] yCoord);
+module ballCtrl(input clk, reset, vCol, hCol, enable, output reg [9:0] xCoord, output reg [8:0] yCoord);
 
     reg vDirection=1;
     reg hDirection=1;
     
-    counter_x_bit #(9,480) vcount(clk, reset, enable, vDirection, yCoord);
-    counter_x_bit #(10,640) hcount(clk, reset, enable, hDirection, xCoord);
+    wire [9:0] hCountOut;
+    wire [8:0] vCountOut;
+    
+    ballCount #(9,480) vcount(clk, reset, enable, vDirection, vCountOut);
+    ballCount #(10,640) hcount(clk, reset, enable, hDirection, hCountOut);
+    
+    always @(posedge clk) begin
+        if (reset | xCoord < 30 | xCoord > 610) begin
+            xCoord <= 320; // Reset to the center
+            yCoord <= 240; // Reset to the center
+        end else if (enable) begin
+            xCoord <= hCountOut;
+            yCoord <= vCountOut;
+        end
+    end
     
     always @(posedge vCol) begin
         vDirection <= ~vDirection;
